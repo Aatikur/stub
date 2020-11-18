f9121c5a7d1a221572d95792500b7cbd1957a3c2
atikrex <atikrex@gmail.com>
2020-11-18 13:39:48 +0530

    first

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Admin;
+use App\Models\User;
+use App\Models\Customer;
+use App\Models\OrderCustomer;
+use App\Models\Product;
+use App\Models\Order;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
+class DashboardController extends Controller
+{
+    public function dashboardView()
+    {
+       
+        return view('admin.dashboard');
+       
+    }
+
+    
+}
+    // function chartData(){
+    //     $data[] = [
+    //         'level' => Carbon::now()->format('Y-m'),
+    //         'delivered' => $this->chartQueryDelivered(Carbon::now()->month),
+    //         'cancel' => $this->chartQueryCancel(Carbon::now()->month),
+    //     ];
+
+    //     for ($i=1; $i < 11; $i++) {
+    //         $data[] = [
+    //             'level' => Carbon::now()->subMonths($i)->format('Y-m'),
+    //             'delivered' => $this->chartQueryDelivered(Carbon::now()->subMonths($i)->month),
+    //             'cancel' => $this->chartQueryCancel(Carbon::now()->subMonths($i)->month),
+    //         ];
+    //     }
+    //     return $data;
+    // }
+
+    // function chartQueryDelivered($month){
+    //     $delivered = Order::where('delivery_status',4) ->whereMonth('created_at', $month)->count();
+    //     return $delivered;
+    // }
+
+    // function chartQueryCancel($month){
+    //     $cancel = Order::where('delivery_status',5) ->whereMonth('created_at', $month)->count();
+    //     return $cancel;
+    // }
+
+    // function pieData($from_date,$to_date,$status)
+    // {
+    //     $data = Order::where('delivery_status',$status)->whereBetween('created_at',[$from_date,$to_date])->count();
+    //     return $data;
+    // }
+
+    // public function changePasswordForm()
+    // {
+    //     return view('admin.change_password');
+    // }
+
+    // public function changePassword(Request $request)
+    // {
+    //     $this->validate($request, [
+    //         'email'   => 'required',
+    //         'current_password' => ['required', 'string'],
+    //         'new_password' => ['required', 'string', 'min:8', 'same:confirm_password'],
+    //     ]);
+
+    //     $user = Admin::where('id',1)->first();
+
+    //     if(Hash::check($request->input('current_password'), $user->password)){
+    //         Admin::where('id',1)->update([
+    //             'email' =>$request->input('email'),
+    //             'password'=>Hash::make($request->input('new_password')),
+    //         ]);
+    //     }else{
+    //         return redirect()->back()->with('error','Sorry Current Password Does Not Correct');
+    //     }
+    // }
+
---
+++ routes/admin.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['namespace' => 'Admin'],function(){
+    Route::get('/admin/login','LoginController@index')->name('admin.login_form');    
+    Route::post('login', 'LoginController@adminLogin');
+   
+
+    Route::group(['middleware'=>'auth:admin','prefix'=>'admin'],function(){
+        Route::get('/dashboard', 'DashboardController@dashboardView')->name('admin.deshboard'); 
+        Route::post('logout', 'LoginController@logout')->name('admin.logout');
+        Route::get('/change/password/form', 'DashboardController@changePasswordForm')->name('admin.change_password_form');
+        Route::post('/change/password', 'DashboardController@changePassword')->name('admin.change_password');
+        
+ 
+        
+    });   
+});
